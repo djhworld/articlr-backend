1ae2f619b9145f57f60dacc81b1122b72aae94a0
Daniel Harper <djharperuk@gmail.com>
2011-02-13 14:27:04 +0000

    fix

--- lib/PhotoEngine.rb
+++ lib/PhotoEngine.rb
@@ -2,7 +2,7 @@ require 'flickraw-cached'
 class PhotoEngine
     def initialize
         @cache_expires = Time.now
-        @flickr_results = {}
+        @flickr_results = []
     end
 
     def search_near_me(lat,long,rad,keywords)
@@ -12,10 +12,9 @@ class PhotoEngine
             photos = flickr.photos.search(:lat => lat, :lon => long, :has_geo=>1, :radius=>5, :radius_units=>"mi", :accuracy => 12, :min_upload_date=> (DateTime.now-(60*60)), :text=>keywords.first) #, :tags=>"gsxsw")
             photos = photos.take(20)
 
+            @flickr_results = []
             photos.each do |ph|
-                owner = mk_owner_url(ph.owner)
-                @flickr_results[owner] = [] if !@flickr_results.key?(owner)
-                @flickr_results[owner] << mkurl(ph.id, ph.farm, ph.server, ph.secret)
+                @flickr_results << mkurl(ph.id, ph.farm, ph.server, ph.secret)
             end
 
             @cache_expires = Time.now+(60*CACHE_EXPIRY)
@@ -23,9 +22,6 @@ class PhotoEngine
         @flickr_results
     end
 
-    def mk_owner_url(owner_id)
-       URI.escape("http://www.flickr.com/photos/#{owner_id}")
-    end
 
     def mkurl(photo_id, photo_farm, photo_server, photo_secret)
        URI.escape("http://farm#{photo_farm}.static.flickr.com/#{photo_server}/#{photo_id}_#{photo_secret}_m.jpg")
