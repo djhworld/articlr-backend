598f60741aaa0a5d94687d3417a883b338c510d7
Daniel Harper <djharperuk@gmail.com>
2011-02-13 14:22:52 +0000

    photo caching

--- lib/PhotoEngine.rb
+++ lib/PhotoEngine.rb
@@ -1,21 +1,37 @@
 require 'flickraw-cached'
 class PhotoEngine
+    def initialize
+        @cache_expires = Time.now
+        @flickr_results = {}
+    end
+
     def search_near_me(lat,long,rad,keywords)
-        FlickRaw.api_key="283720505822a34d2ab61c92f98cfc09"
-        FlickRaw.shared_secret="e6da9a52a9dcaed6"
-        photos = flickr.photos.search(:lat => lat, :lon => long, :has_geo=>1, :radius=>5, :radius_units=>"mi", :accuracy => 12, :min_upload_date=> (DateTime.now-(60*60)), :text=>keywords.first) #, :tags=>"gsxsw")
+        if cache_expired
+            FlickRaw.api_key="283720505822a34d2ab61c92f98cfc09"
+            FlickRaw.shared_secret="e6da9a52a9dcaed6"
+            photos = flickr.photos.search(:lat => lat, :lon => long, :has_geo=>1, :radius=>5, :radius_units=>"mi", :accuracy => 12, :min_upload_date=> (DateTime.now-(60*60)), :text=>keywords.first) #, :tags=>"gsxsw")
+            photos = photos.take(20)
 
-        results = []
+            photos.each do |ph|
+                owner = mk_owner_url(ph.owner)
+                @flickr_results[owner] = [] if !@flickr_results.key?(owner)
+                @flickr_results[owner] << mkurl(ph.id, ph.farm, ph.server, ph.secret)
+            end
 
-        photos = photos.take(20)
-
-        photos.each do |ph|
-            results << mkurl(ph.id, ph.farm, ph.server, ph.secret)
+            @cache_expires = Time.now+(60*CACHE_EXPIRY)
         end
-        results
+        @flickr_results
+    end
+
+    def mk_owner_url(owner_id)
+       URI.escape("http://www.flickr.com/photos/#{owner_id}")
     end
 
     def mkurl(photo_id, photo_farm, photo_server, photo_secret)
-        "http://farm#{photo_farm}.static.flickr.com/#{photo_server}/#{photo_id}_#{photo_secret}_m.jpg"
+       URI.escape("http://farm#{photo_farm}.static.flickr.com/#{photo_server}/#{photo_id}_#{photo_secret}_m.jpg")
+    end
+
+    def cache_expired
+        (@cache_expires < Time.now)
     end
 end
